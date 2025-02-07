e49242e40843826e50e70d8b13b5fd7f22e7bcef
gowtham996 <134760340+gowtham996@users.noreply.github.com>
2025-02-07 11:59:56 +0530

    Add files via upload

---
+++ saibhavyatech/script.js
@@ -0,0 +1,75 @@
+const navId = document.getElementById("nav_menu"),
+  ToggleBtnId = document.getElementById("toggle_btn"),
+  CloseBtnId = document.getElementById("close_btn");
+
+// ==== SHOW MENU ==== //
+ToggleBtnId.addEventListener("click", () => {
+  navId.classList.add("show");
+});
+
+// ==== HIDE MENU ==== //
+CloseBtnId.addEventListener("click", () => {
+  navId.classList.remove("show");
+});
+
+// ==== Animate on Scroll Initialize  ==== //
+AOS.init();
+
+// ==== GSAP Animations (Fast & Smooth) ==== //
+
+// ==== LOGO  ==== //
+gsap.from(".logo", {
+  opacity: 0,
+  y: -10,
+  delay: 0.2,
+  duration: 0.3,
+});
+
+// ==== NAV-MENU ==== //
+gsap.from(".nav_menu_list .nav_menu_item", {
+  opacity: 0,
+  y: -10,
+  delay: 0.3,
+  duration: 0.3,
+  stagger: 0.1, // Faster stagger effect
+});
+
+// ==== TOGGLE BTN ==== //
+gsap.from(".toggle_btn", {
+  opacity: 0,
+  y: -10,
+  delay: 0.3,
+  duration: 0.3,
+});
+
+// ==== MAIN HEADING  ==== //
+gsap.from(".main-heading", {
+  opacity: 0,
+  y: 10,
+  delay: 0.4,
+  duration: 0.5,
+});
+
+// ==== INFO TEXT ==== //
+gsap.from(".info-text", {
+  opacity: 0,
+  y: 10,
+  delay: 0.5,
+  duration: 0.5,
+});
+
+// ==== CTA BUTTONS ==== //
+gsap.from(".btn_wrapper", {
+  opacity: 0,
+  y: 10,
+  delay: 0.5,
+  duration: 0.5,
+});
+
+// ==== TEAM IMAGE ==== //
+gsap.from(".team_img_wrapper img", {
+  opacity: 0,
+  y: 10,
+  delay: 0.6,
+  duration: 0.5,
+});
